2877c7c9f9ca0b64066638bfa792270efc1e1c05
Jarad Gray <jarad.h.gray@gmail.com>
2020-03-01 19:02:58 -0600

    --update

--- RossQuotes/NotifyIconViewModel.cs
+++ RossQuotes/NotifyIconViewModel.cs
@@ -4,9 +4,9 @@ using System.Windows.Input;
 namespace RossQuotes
 {
     /// <summary>
-    /// Provides bindable properties and commands for the NotifyIcon. In this sample, the
-    /// view model is assigned to the NotifyIcon in XAML. Alternatively, the startup routing
-    /// in App.xaml.cs could have created this view model, and assigned it to the NotifyIcon.
+    /// Provides bindable properties and commands for the NotifyIcon. In this app, the view model
+    /// is created and assigned to the NotifyIcon in the startup routing of App.xaml.cs.
+    /// Alternatively, the view model could have been assigned to the NotifyIcon via XAML in App.xaml.
     /// </summary>
     public class NotifyIconViewModel
     {
