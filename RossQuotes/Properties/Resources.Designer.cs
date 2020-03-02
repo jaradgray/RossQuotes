a4cec5c3c4a054f45b25288be428214f774660b1
Jarad Gray <jarad.h.gray@gmail.com>
2020-03-01 20:23:53 -0600

    add icon_steves_128 icon resource

--- RossQuotes/Properties/Resources.Designer.cs
+++ RossQuotes/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace RossQuotes.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon icon_steves_128 {
+            get {
+                object obj = ResourceManager.GetObject("icon_steves_128", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
